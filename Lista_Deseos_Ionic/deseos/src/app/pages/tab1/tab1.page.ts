a3c27622b4c823cebc2cbd07ccf72456f5f5f8b3
Eduardo Caldera Coltters <e.calderac@hotmail.com>
2020-12-01 19:01:08 -0300

    app ionic lista de deseos finalizada

--- Lista_Deseos_Ionic/deseos/src/app/pages/tab1/tab1.page.ts
+++ Lista_Deseos_Ionic/deseos/src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { DeseosService } from '../../services/deseos.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,48 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    public deseosService: DeseosService,
+    private router: Router,
+    private alertCtrl: AlertController
+  ) {}
+
+    async agregarLista(){
+      const alert = await this.alertCtrl.create({
+        header: 'Nueva lista',
+        inputs: [
+          {
+            name: 'titulo',
+            type:'text',
+            placeholder:'Nombre de la lista'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Crear',
+            handler: (data) => {
+              console.log(data);
+              if(data.titulo.length === 0){
+                return;
+              }
+
+              const listaId = this.deseosService.crearLista(data.titulo);
+              //CREAR LISTA
+              this.router.navigateByUrl(`tabs/tab1/agregar/${listaId}`);
+            }
+          }, 
+          {
+            text: 'Cancelar',
+            role: 'cancel',
+            handler: ()=> {
+              console.log('Cancelar');
+            }
+          }
+        ]
+      });
+  
+      await alert.present();
+    }
+
 
 }
